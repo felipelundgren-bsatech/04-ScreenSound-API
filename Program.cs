4183cb906277107448359405481b6d54f6b63f09
felipelundgren-bsatech <felipe.lundgren@bsatech.io>
2025-10-02 14:02:27 -0300

    B: Aplicando API

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine();
+﻿using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(resposta);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+    }
+}
+
+
--- obj/Debug/net9.0/04-ScreenSound-API.AssemblyInfo.cs
+++ obj/Debug/net9.0/04-ScreenSound-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("04-ScreenSound-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07005ba05d47dbc1bc5611312ed7191938348316")]
 [assembly: System.Reflection.AssemblyProductAttribute("04-ScreenSound-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("04-ScreenSound-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
