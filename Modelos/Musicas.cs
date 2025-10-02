0ccee5490c8eea53d2a96a95c494e13a39522717
felipelundgren-bsatech <felipe.lundgren@bsatech.io>
2025-10-02 14:43:15 -0300

    C: Deserializando API

---
+++ Modelos/Musicas.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection.Metadata;
+using System.Text.Json.Serialization;
+
+namespace _04_ScreenSound_API.Modelos;
+
+internal class Musica
+{
+    [JsonPropertyName("song")]
+    public string Nome { get; set; }
+    [JsonPropertyName("artist")]
+    public string Artista { get; set; }
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+    [JsonPropertyName("genre")]
+    public string Genero { get; set; }
+
+    public void ExibirDetalhesDaMusica()
+    {
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Musica: {Nome}");
+        Console.WriteLine($"Duração: {Duracao/1000} em segundos");
+        Console.WriteLine($"Genero Musical: {Genero}");
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-﻿using (HttpClient client = new HttpClient())
+﻿using _04_ScreenSound_API.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
 {
     try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        Console.WriteLine(musicas.Count);
+
+        musicas[1998].ExibirDetalhesDaMusica();
     }
     catch (Exception ex)
     {
